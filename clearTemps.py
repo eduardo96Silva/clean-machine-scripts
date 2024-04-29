46e34a1cdb86a42ff16ff5844b7f2963fa48fa9c
eduardo96Silva <eduardo9631s@gmail.com1>
2024-04-29 17:37:32 -0300

    comentários adicionados

--- clearTemps.py
+++ clearTemps.py
@@ -1,12 +1,13 @@
 import os
 import shutil
 
+# Função para fazer a limpeza das subpastas e arquivos que contém na pasta alvo
 def limpar_pasta(pasta):
     for root, dirs, files in os.walk(pasta, topdown=False):
         for file in files:
             try:
                 os.remove(os.path.join(root, file))
-            except (PermissionError, OSError):
+            except (PermissionError, OSError): # Os except's -> vai para o proximo item do loop, caso haja algum arquivo sendo usado pelo sistema.
                 continue
         for dir in dirs:
             try:
@@ -14,10 +15,12 @@ def limpar_pasta(pasta):
             except (PermissionError, OSError):
                 continue
 
+# Lista de caminhos para diretórios que contém arquivos temporários:
 paths = [
     "C:/Windows/Temp",
-    "C:/Users/eduar/AppData/Local/Temp"
+    "C:/Users/eduar/AppData/Local/Temp" # Nesse caminho substitua o usuário para o seu usuário !
 ]
 
+# Acionando a função para cada path da lista
 for path in paths:
     limpar_pasta(path)
